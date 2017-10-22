cc296bae940d76dc069426a3592d497786670b47
andrew-secret <andre.rusakow@jimdo.com>
2017-10-22 23:03:53 +0200

    feat(todo-app): dispatching actions with ActionCreators

--- src/reducer/todo.js
+++ src/reducer/todo.js
@@ -8,12 +8,17 @@ const initialState = {
     currentTodo: '',
 };
 
+const TODO_ADD = 'TODO_ADD';
+const CURRENT_UPDATE = 'CURRENT_UPDATE';
+
+export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val});
+
 export default (state = initialState, action) => {
     switch(action.type) {
-        case 'TODO_ADD':
+        case TODO_ADD:
             return {...state, todos: state.todos.concat(action.payload)}
         
-        case 'CURRENT_UPDATE':
+        case CURRENT_UPDATE:
             return {...state, currentTodo: action.payload}
         default:
             return state;
