1d6857f910804833bc8cb3de27ad0b4d4dfe1259
andrew-secret <andre.rusakow@jimdo.com>
2017-10-22 23:05:57 +0200

    feat(todo-app): take value and call action creator with bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import store from "./store";
+import {bindActionCreators} from 'redux';
+import {updateCurrent} from "./reducer/todo";
 
-const todoChangeHandler = (val) => store.dispatch({type: 'CURRENT_UPDATE', payload: val});
+const actions = bindActionCreators({updateCurrent}, store.dispatch);
 
 const render = () => {
-    const state = store.getState();
-    ReactDOM.render(<App todos={state.todos} 
-        changeCurrent={todoChangeHandler}
-        currentTodo={state.currentTodo}/>, 
-        document.getElementById('root'));
-}
+  const state = store.getState();
+  ReactDOM.render(
+    <App
+        todos={state.todos}
+        changeCurrent={actions.updateCurrent}
+        currentTodo={state.currentTodo}
+    />,
+    document.getElementById("root")
+  );
+};
 render();
 
 store.subscribe(render);
