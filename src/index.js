279ec05a039d3cd0ed789929e74f508b37e9d724
André Rusakow <andre.rusakow@jimdo.com>
2017-10-23 15:20:07 +0200

    Implement react redux (#8)
    * feat(todoApp): add react-redux
    
    * feat(todoApp): implement provider componenent and connect redux store to the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -9,19 +10,12 @@ import {updateCurrent} from "./reducer/todo";
 
 const actions = bindActionCreators({updateCurrent}, store.dispatch);
 
-const render = () => {
-  const state = store.getState();
-  ReactDOM.render(
-    <App
-        todos={state.todos}
-        changeCurrent={actions.updateCurrent}
-        currentTodo={state.currentTodo}
-    />,
-    document.getElementById("root")
-  );
-};
-render();
 
-store.subscribe(render);
+ReactDOM.render(
+    <Provider store={store}>
+        <App changeCurrent={actions.updateCurrent}/>
+    </Provider>,
+    document.getElementById("root")
+);
 
 registerServiceWorker();
